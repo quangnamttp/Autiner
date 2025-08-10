8e8146ed3a1bf278c4c9b9efc6feaa00941d3695
quangnamttp <quangnamttp@gmail.com>
2025-08-10 09:08:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 import os
-from bots.telegram_bot.py import build_app  # noqa
+from bots.telegram_bot import build_app
 
 if __name__ == "__main__":
     # Yêu cầu: đặt biến môi trường trước (TELEGRAM_BOT_TOKEN, ALLOWED_USER_ID, TZ_NAME)
