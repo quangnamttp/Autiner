54fd844b7b7507a9dc99a1c45c6875e932f8f30d
quangnamttp <quangnamttp@gmail.com>
2025-08-16 21:19:28 +0700

    Update time_utils.py

--- autiner_bot/utils/time_utils.py
+++ autiner_bot/utils/time_utils.py
@@ -1,13 +1,25 @@
-# autiner_bot/utils/time_utils.py
 from datetime import datetime
 import pytz
 from autiner_bot.settings import S
 
-def get_vietnam_time():
-    tz = pytz.timezone(S.TZ_NAME)
-    return datetime.now(tz)
+# Timezone VN lấy từ settings
+VN_TZ = pytz.timezone(S.TZ_NAME)
 
-def format_time(dt=None):
+def get_vietnam_time() -> datetime:
+    """Lấy thời gian hiện tại theo múi giờ VN"""
+    return datetime.now(VN_TZ)
+
+def format_time(dt: datetime | None = None, fmt: str = "%H:%M:%S %d-%m-%Y") -> str:
+    """
+    Format thời gian theo định dạng tùy chọn.
+    - Default: 'HH:MM:SS dd-mm-YYYY'
+    """
     if dt is None:
         dt = get_vietnam_time()
-    return dt.strftime("%H:%M:%S %d-%m-%Y")
+    return dt.strftime(fmt)
+
+def utc_to_vietnam(utc_dt: datetime) -> datetime:
+    """Chuyển từ UTC datetime sang VN datetime"""
+    if utc_dt.tzinfo is None:
+        utc_dt = pytz.utc.localize(utc_dt)
+    return utc_dt.astimezone(VN_TZ)
